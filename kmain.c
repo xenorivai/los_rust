6c166bb4421600971252811981cba92fce744571
ChanBong <harsh_k@ma.iitr.ac.in>
2022-01-22 01:23:40 +0530

    Finish Chapter 4

--- kmain.c
+++ kmain.c
@@ -1,8 +1,28 @@
-int sum_of_three(int arg1, int arg2, int arg3){
-    return arg1 + arg2 + arg3;
+#include "fb.h"
+#include "serial_port.h"
+
+#define SERIAL_COM1_PORT 0x3F8
+
+void configure_default(unsigned int port){
+    serial_configure_baud_rate(port, 2);
+    serial_configure_line(port);
+    serial_configure_fifo(port);
+    serial_configure_modem(port);
+
 }
 
 int main(){
-    sum_of_three(3, 2, 1);
+    unsigned int len = 10;
+    char buf[10] = "Hello Neo!";
+
+    fb_clear(FB_MAGENTA);
+    fb_write(buf, len);
+
+    configure_default(SERIAL_COM1_PORT);
+
+    if (serial_is_transmit_fifo_empty(SERIAL_COM1_PORT)!=0){
+        outb(SERIAL_COM1_PORT, 'A');
+    }
+
     return 0;
 }
--- loader.s
+++ loader.s
@@ -21,15 +21,9 @@ align 4                         ; the code must be 4 byte aligned
     mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                 ; stack (end of memory area)
 
-extern sum_of_three         ; the function sum_of_three is defined elsewhere
+extern main         ; the function sum_of_three is defined elsewhere
 
 loader:                     ; the loader label (defined as entry point in the linker script)
-    mov eax, 0xCAFEBABE     ; place the number 0xCAFEBABE in the register eax
-    ; The assembly code
-    push dword 3                ; arg3
-    push dword 2                ; arg2
-    push dword 1                ; arg1
-    call sum_of_three           ; call the function, the result will be in eax
-
+    call main
 .loop:
     jmp .loop               ; loop forever
